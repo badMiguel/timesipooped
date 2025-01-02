be71b746f38689669d8af703155ebdfcd926ba14
MiguelBad <badayosbiboy@gmail.com>
2025-01-03 00:50:17 +0930

    oauth google  setup

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	// "encoding/json"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	// todo remove on production
@@ -52,36 +55,110 @@ func newUser(u string) *User {
 	}
 }
 
-func get(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
+func handleGetReq(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusBadRequest)
 	}
 }
 
-func post(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
+func handlePostReq(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusBadRequest)
 	}
 }
 
 func newOAuthConfig() *OAuthConfig {
 	config := &OAuthConfig{}
+	config.ClientId = os.Getenv("CLIENT_ID")
+	config.ClientSecret = os.Getenv("CLIENT_SECRET")
+	config.RedirectURI = os.Getenv("REDIRECT_URI")
+	config.OauthURL = "https://accounts.google.com/o/oauth2/auth"
+	config.TokenURL = "https://accounts.google.com/o/oauth2/token"
+	config.UserInfoURL = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
+
+	return config
+}
+
+func handleLogin(authConf *OAuthConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		authUrl := fmt.Sprintf(
+			"%s?response_type=code&client_id=%s&redirect_uri=%s&scope=openid%%20email",
+			authConf.OauthURL,
+			authConf.ClientId,
+			url.QueryEscape(authConf.RedirectURI),
+		)
+		http.Redirect(w, r, authUrl, http.StatusFound)
+	}
+}
+
+func handleCallback(authConf *OAuthConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			http.Error(w, "Missing code parameter", http.StatusBadRequest)
+			return
+		}
+
+		resp, err := http.PostForm(authConf.TokenURL, url.Values{
+			"code":          {code},
+			"client_id":     {authConf.ClientId},
+			"client_secret": {authConf.ClientSecret},
+			"redirect_uri":  {authConf.RedirectURI},
+			"grant_type":    {"authorization_code"},
+		})
+		if err != nil {
+			log.Printf("Error exchanging code for token: %v\n", err)
+			http.Error(w, "Failed to exchange code", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		var tokenData map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&tokenData); err != nil {
+			http.Error(w, "Failed to decode token response", http.StatusInternalServerError)
+			return
+		}
+
+		accessToken := tokenData["access_token"].(string)
+
+		req, err := http.NewRequest("GET", authConf.UserInfoURL, nil)
+		if err != nil {
+			log.Printf("Error creating user info request: %v\n", err)
+			http.Error(w, "Failed to create user info request", http.StatusInternalServerError)
+		}
+		req.Header.Set("Authorization", "Bearer "+accessToken)
+		client := &http.Client{}
+		resp, err = client.Do(req)
+		if err != nil {
+			log.Printf("Error fetching user info: %v\n", err)
+			http.Error(w, "Failed to fetch user info", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		var userInfo map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
+			log.Printf("Error decoding user info: %v\n", err)
+			http.Error(w, "Failed to decode user info", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "User Info: %v", userInfo)
+	}
+}
+
+func main() {
 	err := godotenv.Load("../.env")
 	if err != nil {
 		panic(err)
 	}
-	config.ClientId = os.Getenv("CLIENT_ID")
-	config.ClientSecret = os.Getenv("CLIENT_SECRET")
-	config.RedirectURI = "http://localhost:8080/callback"
-	config.OauthURL = "https://accounts.google.com/o/oauth2/auth"
-	config.TokenURL = "https://accounts.google.com/o/oauth2/token"
-	config.UserInfoURL = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
-	return config
-}
 
-func main() {
-	http.HandleFunc("/get", get)
-	http.HandleFunc("/post", post)
+	authConf := newOAuthConfig()
+	http.HandleFunc("/login", handleLogin(authConf))
+	http.HandleFunc("/login/callback", handleCallback(authConf))
 
-	log.Fatal(http.ListenAndServe(":1234", nil))
+	http.HandleFunc("/get", handleGetReq)
+	http.HandleFunc("/post", handlePostReq)
+
+	log.Fatal(http.ListenAndServe(os.Getenv("SERVER_PORT"), nil))
 }
