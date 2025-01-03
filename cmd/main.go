0d426077629914f863e308f1c85492b073f955a9
MiguelBad <badayosbiboy@gmail.com>
2025-01-03 15:34:00 +0930

    initialise table

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -17,20 +18,47 @@ var (
 	userInfo = make(map[string]*internal.UserInfo)
 )
 
-func connectDB() bool {
+func connectDB() error {
 	db, err := sql.Open("sqlite3", "../internal/poop.db")
 	if err != nil {
-		log.Printf("Error opening/creating sqlite3 file: %v\n", err)
-		return false
+		return fmt.Errorf("Error opening/creating sqlite3 file: %v\n", err)
 	}
 	defer db.Close()
 
 	if err := db.Ping(); err != nil {
-		log.Printf("Error connecting to the sqlite3 file: %v\n", err)
-		return false
+		return fmt.Errorf("Error connecting to the sqlite3 file: %v\n", err)
 	}
 
-	return true
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+        userId INTEGER PRIMARY KEY,
+        email TEXT UNIQUE,
+        givenName TEXT, 
+        familyName TEXT, 
+        joined DATETIME DEFAULT CURRENT_TIMESTAMP,
+        poopTotal INTEGER DEFAULT 0,
+        failedTotal INTEGER DEFAULT 0
+    )`)
+	if err != nil {
+		return fmt.Errorf(fmt.Sprintf("Error creating new users table: %v\n", err))
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS poop (
+        poopId INTEGER PRIMARY KEY AUTOINCREMENT,
+        date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        userId INTEGER,
+        FOREIGN KEY(userId) REFERENCES users(userId) ON DELETE CASCADE
+    )`)
+	if err != nil {
+		return fmt.Errorf("Error creating new poop table: %v\n", err)
+	}
+
+	return nil
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	for i := range userInfo {
+		fmt.Fprintln(w, *userInfo[i])
+	}
 }
 
 func main() {
@@ -39,12 +67,20 @@ func main() {
 		panic(err)
 	}
 
-	for i := 0; i < 3; {
-		if success := connectDB(); success {
+	for i := 0; i < 10; {
+		if err := connectDB(); err == nil {
+			log.Println("Successfully connected to the database")
 			break
+		} else {
+			log.Println(err)
 		}
+
 		i++
 		log.Printf("Attempt %d failed. Trying again...\n", i)
+
+		if i == 3 {
+			panic("Maximum attempts reached. Could not connect to DB")
+		}
 	}
 
 	authConf := internal.NewOAuthConfig()
@@ -52,6 +88,8 @@ func main() {
 	http.HandleFunc("/login/callback", internal.HandleCallback(authConf, userInfo))
 
 	http.HandleFunc("/poop/add", internal.AddPoop)
+
+	http.HandleFunc("/", test)
 
 	log.Fatal(http.ListenAndServe(os.Getenv("SERVER_PORT"), nil))
 }
