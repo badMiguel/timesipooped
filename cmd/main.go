ca5b7e733c4c5ebe7e1ea0a815d9fc68ed2fc0c7
MiguelBad <badayosbiboy@gmail.com>
2025-01-03 15:46:49 +0930

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,43 +17,6 @@ var (
 	userInfo = make(map[string]*internal.UserInfo)
 )
 
-func connectDB() error {
-	db, err := sql.Open("sqlite3", "../internal/poop.db")
-	if err != nil {
-		return fmt.Errorf("Error opening/creating sqlite3 file: %v\n", err)
-	}
-	defer db.Close()
-
-	if err := db.Ping(); err != nil {
-		return fmt.Errorf("Error connecting to the sqlite3 file: %v\n", err)
-	}
-
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
-        userId INTEGER PRIMARY KEY,
-        email TEXT UNIQUE,
-        givenName TEXT, 
-        familyName TEXT, 
-        joined DATETIME DEFAULT CURRENT_TIMESTAMP,
-        poopTotal INTEGER DEFAULT 0,
-        failedTotal INTEGER DEFAULT 0
-    )`)
-	if err != nil {
-		return fmt.Errorf(fmt.Sprintf("Error creating new users table: %v\n", err))
-	}
-
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS poop (
-        poopId INTEGER PRIMARY KEY AUTOINCREMENT,
-        date DATETIME DEFAULT CURRENT_TIMESTAMP,
-        userId INTEGER,
-        FOREIGN KEY(userId) REFERENCES users(userId) ON DELETE CASCADE
-    )`)
-	if err != nil {
-		return fmt.Errorf("Error creating new poop table: %v\n", err)
-	}
-
-	return nil
-}
-
 func test(w http.ResponseWriter, r *http.Request) {
 	for i := range userInfo {
 		fmt.Fprintln(w, *userInfo[i])
@@ -67,25 +29,11 @@ func main() {
 		panic(err)
 	}
 
-	for i := 0; i < 10; {
-		if err := connectDB(); err == nil {
-			log.Println("Successfully connected to the database")
-			break
-		} else {
-			log.Println(err)
-		}
-
-		i++
-		log.Printf("Attempt %d failed. Trying again...\n", i)
-
-		if i == 3 {
-			panic("Maximum attempts reached. Could not connect to DB")
-		}
-	}
-
 	authConf := internal.NewOAuthConfig()
 	http.HandleFunc("/login", internal.HandleLogin(authConf))
 	http.HandleFunc("/login/callback", internal.HandleCallback(authConf, userInfo))
+
+	internal.ConnectDB()
 
 	http.HandleFunc("/poop/add", internal.AddPoop)
 
---
+++ internal/db.go
@@ -0,0 +1,66 @@
+package internal
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func newConnection() error {
+	db, err := sql.Open("sqlite3", "../internal/poop.db")
+	if err != nil {
+		return fmt.Errorf("Error opening/creating sqlite3 file: %v\n", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		return fmt.Errorf("Error connecting to the sqlite3 file: %v\n", err)
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+        userId INTEGER PRIMARY KEY,
+        email TEXT UNIQUE,
+        givenName TEXT, 
+        familyName TEXT, 
+        joined DATETIME DEFAULT CURRENT_TIMESTAMP,
+        poopTotal INTEGER DEFAULT 0,
+        failedTotal INTEGER DEFAULT 0
+    )`)
+	if err != nil {
+		return fmt.Errorf(fmt.Sprintf("Error creating new users table: %v\n", err))
+	}
+	log.Println("Created users table")
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS poop (
+        poopId INTEGER PRIMARY KEY AUTOINCREMENT,
+        date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        userId INTEGER,
+        FOREIGN KEY(userId) REFERENCES users(userId) ON DELETE CASCADE
+    )`)
+	if err != nil {
+		return fmt.Errorf("Error creating new poop table: %v\n", err)
+	}
+	log.Println("Created poop table")
+
+	return nil
+}
+
+func ConnectDB() {
+	for i := 0; i < 10; {
+		if err := newConnection(); err == nil {
+			log.Println("Successfully connected to the database")
+			break
+		} else {
+			log.Println(err)
+		}
+
+		i++
+		log.Printf("Attempt %d failed. Trying again...\n", i)
+
+		if i == 10 {
+			panic("Maximum attempts reached. Could not connect to DB")
+		}
+	}
+}
