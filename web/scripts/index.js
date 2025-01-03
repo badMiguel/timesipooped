871e093f49666c038cdb144fbce865d7f469ddd9
MiguelBad <badayosbiboy@gmail.com>
2025-01-03 23:46:56 +0930

    client side setup

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,34 +1,43 @@
-function addFailPoop() {}
+async function addFailPoop() {}
 
-function addPoop() {}
+async function addPoop() {}
 
-function fetchVal() {}
+async function loading() {}
 
-function main() {
+async function fetchVal() {}
+
+async function login() {}
+
+async function main() {
+    const profilePic = document.querySelector(".profile-picture--container");
+    if (!(profilePic instanceof HTMLDivElement)) {
+        console.error(`failed to find profile-picture--container element.`);
+        return;
+    }
+    profilePic.addEventListener("click", async () => {
+        await login();
+        await fetchVal();
+    });
+
     const poopButton = document.querySelector(".poop--button");
     if (!(poopButton instanceof HTMLParagraphElement)) {
-        // todo: add error handling
         console.error(`failed to find poop-button element.`);
         return;
     }
-
-    const fPoopButton = document.querySelector(".failed-poop--button");
-    if (!(fPoopButton instanceof HTMLParagraphElement)) {
-        // todo: add error handling
-        console.error(`failed to find failed-poop--button element.`);
-        return;
-    }
-
     poopButton.addEventListener("click", () => {
         addPoop();
     });
 
+    const fPoopButton = document.querySelector(".failed-poop--button");
+    if (!(fPoopButton instanceof HTMLParagraphElement)) {
+        console.error(`failed to find failed-poop--button element.`);
+        return;
+    }
     fPoopButton.addEventListener("click", () => {
         addFailPoop();
     });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetchVal();
     main();
 });
